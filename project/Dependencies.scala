a6a23288349d7bb8df0d24d39b78ed3c72d46ba6
Peter Mortier <peter.mortier@gmail.com>
2018-03-26 07:56:36 +0200

    upgrade dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,22 +2,22 @@ import sbt._
 
 object Dependencies {
 
-  val refinedVersion = "0.8.4"
+  val refinedVersion = "0.8.7"
   val play25Version  = "2.5.16"
-  val play26Version  = "2.6.7"
+  val play26Version  = "2.6.12"
 
   val refined   =         "eu.timepit"                 %%    "refined"                  % refinedVersion
 
   val refinedScalacheck = "eu.timepit"                 %%    "refined-scalacheck"       % refinedVersion   % "test"
-  val scalaTest =         "org.scalatest"              %%    "scalatest"                % "3.0.3"   % "test"
+  val scalaTest =         "org.scalatest"              %%    "scalatest"                % "3.0.5"   % "test"
   val scalaCheck =        "org.scalacheck"             %%    "scalacheck"               % "1.13.5"  % "test"
 
-  val playWsJson          = "com.typesafe.play"          %%    "play-ws-standalone-json"  % "1.1.2"  % "test"
+  val playWsJson          = "com.typesafe.play"          %%    "play-ws-standalone-json"  % "1.1.6"  % "test"
 
-  val play26Json =          "com.typesafe.play"          %%    "play-json"                % play26Version
+  val play26Json =          "com.typesafe.play"          %%    "play-json"                % "2.6.9"
   val play26 =              "com.typesafe.play"          %%    "play"                     % play26Version
   val play26NettyServer   = "com.typesafe.play"          %%    "play-netty-server"        % play26Version  % "test"
-  val scalaTestPlusPlay26 = "org.scalatestplus.play"     %%    "scalatestplus-play"       % "3.1.0"  % "test"
+  val scalaTestPlusPlay26 = "org.scalatestplus.play"     %%    "scalatestplus-play"       % "3.1.2"  % "test"
 
   val play25Json =          "com.typesafe.play"          %%    "play-json"                % play25Version
   val play25 =              "com.typesafe.play"          %%    "play"                     % play25Version
