9d4f19341e993d99c328fe991698f2e15f00ffd5
Scala steward <43047562+scala-steward@users.noreply.github.com>
2018-11-06 23:45:57 +0100

    Update filters-helpers, play, play-akka-http-server... to 2.6.20 (#7)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   val refinedVersion = "0.9.2"
   val play25Version  = "2.5.18"
-  val play26Version  = "2.6.17"
+  val play26Version  = "2.6.20"
   val play27Version  = "2.7.0-M2"
 
   val refined   =         "eu.timepit"                 %%    "refined"                  % refinedVersion
