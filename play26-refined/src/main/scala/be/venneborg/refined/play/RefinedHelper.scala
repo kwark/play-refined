372a80896ff5f9ee7dfea833807c0311095d031d
Peter Mortier <peter.mortier@gmail.com>
2019-02-01 00:40:27 +0100

    make it compile again on 2.11

--- play26-refined/src/main/scala/be/venneborg/refined/play/RefinedHelper.scala
+++ play26-refined/src/main/scala/be/venneborg/refined/play/RefinedHelper.scala
@@ -7,7 +7,7 @@ object RefinedHelper {
   private[play] def handleBound[T, P](bound: Either[String, T])
                                      (implicit reftype: RefType[Refined],
                                       validate: Validate[T, P]): Either[String, Refined[T, P]] = {
-    bound.flatMap { v =>
+    bound.right.flatMap { v =>
       reftype.refine[P](v) match {
         case Right(valueP) => Right(valueP)
         case l @ Left(_)   => l
