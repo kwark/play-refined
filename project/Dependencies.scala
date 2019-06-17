74256a1a0b676fe037e9244ae89decd3a4554164
Scala Steward <me@scala-steward.org>
2019-06-17 20:31:33 +0200

    Update play-ws-standalone-json to 2.0.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
   val refinedScalacheck = "eu.timepit"                 %%    "refined-scalacheck"       % refinedVersion   % "test"
   val scalaCheck        = "org.scalacheck"             %%    "scalacheck"               % "1.14.0"  % "test"
 
-  val play26WsJson        = "com.typesafe.play"          %%    "play-ws-standalone-json"  % "2.0.4"  % "test"
+  val play26WsJson        = "com.typesafe.play"          %%    "play-ws-standalone-json"  % "2.0.6"  % "test"
   val play26Json          = "com.typesafe.play"          %%    "play-json"                % "2.6.13"
   val play26              = "com.typesafe.play"          %%    "play"                     % play26Version
   val play26NettyServer   = "com.typesafe.play"          %%    "play-netty-server"        % play26Version  % "test"
@@ -26,7 +26,7 @@ object Dependencies {
   val play25Ws            = "com.typesafe.play"          %%    "play-ws"                  % play25Version  % "test"
   val play25Test          = "com.typesafe.play"          %%    "play-test"                % play25Version  % "test"
 
-  val play27WsJson        = "com.typesafe.play"          %%    "play-ws-standalone-json"  % "2.0.4"  % "test"
+  val play27WsJson        = "com.typesafe.play"          %%    "play-ws-standalone-json"  % "2.0.6"  % "test"
   val play27Json          = "com.typesafe.play"          %%    "play-json"                % "2.7.1"
   val play27              = "com.typesafe.play"          %%    "play"                     % play27Version
   val play27NettyServer   = "com.typesafe.play"          %%    "play-netty-server"        % play27Version  % "test"
