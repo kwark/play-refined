90d4d56f72830af788108c9491332651595ab2e8
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2019-09-29 12:19:50 +0200

    Update refined to 0.9.10 (#75)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val refinedVersion = "0.9.9"
+  val refinedVersion = "0.9.10"
   val play25Version  = "2.5.19"
   val play26Version  = "2.6.23"
   val play27Version  = "2.7.3"
