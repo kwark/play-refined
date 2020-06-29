0550e3f1d4ef5bf7e9eb1884f58de349d017a824
Scala Steward <me@scala-steward.org>
2020-06-29 14:29:56 +0200

    Update play, play-netty-server to 2.8.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val refinedVersion = "0.9.14"
-  val play26Version  = "2.6.25"
+  val play26Version  = "2.8.2"
   val play27Version  = "2.7.5"
   val play28Version  = "2.8.2"
 
