1170a009fdda13d4d646cae5666795b8755c62b7
Peter Mortier <kwark@users.noreply.github.com>
2020-09-11 08:54:14 +0200

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,8 +12,14 @@ object Dependencies {
   val refinedScalacheck = "eu.timepit"                 %%    "refined-scalacheck"       % refinedVersion   % "test"
   val scalaCheck        = "org.scalacheck"             %%    "scalacheck"               % "1.14.3"  % "test"
 
+  val play28WsJson        = "com.typesafe.play"          %%    "play-ws-standalone-json"  % "2.1.2"  % "test"
+  val play28Json          = "com.typesafe.play"          %%    "play-json"                % "2.9.1"
+  val play28              = "com.typesafe.play"          %%    "play"                     % play28Version
+  val play28NettyServer   = "com.typesafe.play"          %%    "play-netty-server"        % play28Version  % "test"
+  val scalaTestPlusPlay28 = "org.scalatestplus.play"     %%    "scalatestplus-play"       % "4.0.3"  % "test"
+
   val play26WsJson        = "com.typesafe.play"          %%    "play-ws-standalone-json"  % "2.0.8"  % "test"
-  val play26Json          = "com.typesafe.play"          %%    "play-json"                % "2.9.1"
+  val play26Json          = "com.typesafe.play"          %%    "play-json"                % "2.6.14"
   val play26              = "com.typesafe.play"          %%    "play"                     % play26Version
   val play26NettyServer   = "com.typesafe.play"          %%    "play-netty-server"        % play26Version  % "test"
   val scalaTestPlusPlay26 = "org.scalatestplus.play"     %%    "scalatestplus-play"       % "3.1.3"  % "test"
@@ -24,12 +30,6 @@ object Dependencies {
   val play27NettyServer   = "com.typesafe.play"          %%    "play-netty-server"        % play27Version  % "test"
   val scalaTestPlusPlay27 = "org.scalatestplus.play"     %%    "scalatestplus-play"       % "4.0.3"  % "test"
   val play27NettyUnix     = "io.netty" % "netty-transport-native-unix-common" % "4.1.51.Final" % "test"
-
-  val play28WsJson        = "com.typesafe.play"          %%    "play-ws-standalone-json"  % "2.1.2"  % "test"
-  val play28Json          = "com.typesafe.play"          %%    "play-json"                % "2.9.0"
-  val play28              = "com.typesafe.play"          %%    "play"                     % play28Version
-  val play28NettyServer   = "com.typesafe.play"          %%    "play-netty-server"        % play28Version  % "test"
-  val scalaTestPlusPlay28 = "org.scalatestplus.play"     %%    "scalatestplus-play"       % "4.0.3"  % "test"
 
   val testDependencies = Seq(scalaCheck)
   val play28Dependencies = Seq(refined, play28, play28Json, play28NettyServer, scalaTestPlusPlay28, play28WsJson)
