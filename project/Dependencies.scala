91ece91aa9cbd035506a6f353b67312b1ce37297
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-11-11 18:30:54 +0100

    Update play-netty-server to 2.8.5 (#145)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val refinedVersion = "0.9.17"
   val play26Version  = "2.6.25"
   val play27Version  = "2.7.7"
-  val play28Version  = "2.8.2"
+  val play28Version  = "2.8.5"
 
   val refined   =         "eu.timepit"                 %%    "refined"                  % refinedVersion
 
